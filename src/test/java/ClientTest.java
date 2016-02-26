c44b4ba7adb217e2b830b34fca7ed7907562ebfd
Michelle Brecunier <michelle.brecunier@gmail.com>
2016-02-26 08:58:44 -0800

    test that Client object instantiates correctly

--- src/test/java/ClientTest.java
+++ src/test/java/ClientTest.java
@@ -8,6 +8,12 @@ public class ClientTest {
   public DatabaseRule database = new DatabaseRule();
 
   @Test
+  public void client_instantiatesCorrectly_true() {
+    Client newClient = new Client("Darlene", 1);
+    assertTrue(newClient instanceof Client);
+  }
+
+  @Test
   public void all_emptyAtFirst() {
     assertEquals(Client.all().size(), 0);
   }
